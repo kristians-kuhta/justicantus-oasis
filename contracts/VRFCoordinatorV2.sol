2fa8077674781adeb9d54501c97f365eb91704e6
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-08-07 17:48:34 +0100

    Remove unused VRF coordinator related contracts

--- contracts/VRFCoordinatorV2.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
-
-import { VRFCoordinatorV2 } from "@chainlink/contracts/src/v0.8/VRFCoordinatorV2.sol";
--- contracts/VRFCoordinatorV2Mock.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
-
-import { VRFCoordinatorV2Mock } from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
