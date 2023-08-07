92d73ab5776deb02e4671d108dea6147b98bf7ba
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-08-07 16:26:31 +0300

    Remove chainlink stuff from deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -37,63 +37,20 @@ function saveFrontendFiles(platform) {
 async function main() {
   const Platform = await hre.ethers.getContractFactory('Platform');
 
-  const {
-    VRF_COORDINATOR,
-    SUBSCRIPTION_ID,
-    KEY_HASH,
-    VRF_ADMIN,
-    NODE_ENV
-  } = process.env;
-
-  let coordinator;
-  let subscriptionId;
-
-  if (hre.network.name === 'localhost') {
-    const vrfAdmin = await ethers.getImpersonatedSigner(VRF_ADMIN);
-
-    const BASE_FEE = '100000000000000000';
-    const GAS_PRICE_LINK = '1000000000';
-    const SUBSCRIPTION_BALANCE = '10000000000000000000'; // 10 LINK
-
-    const Coordinator = await ethers.getContractFactory("VRFCoordinatorV2Mock");
-    coordinator = await Coordinator.deploy(BASE_FEE, GAS_PRICE_LINK);
-    await coordinator.deployed();
-
-    // Create the subscription
-    const createSubResponse = await coordinator.connect(vrfAdmin).createSubscription();
-    const subTx = await createSubResponse.wait();
-    subscriptionId = subTx.events[0].args.subId;
-
-    // Fund the subscription
-    await (await coordinator.connect(vrfAdmin).fundSubscription(subscriptionId, SUBSCRIPTION_BALANCE)).wait();
-  } else {
-    const Coordinator = await ethers.getContractFactory("VRFCoordinatorV2");
-    coordinator = await Coordinator.attach(VRF_COORDINATOR);
-    subscriptionId = SUBSCRIPTION_ID;
-  }
+  const { NODE_ENV } = process.env;
 
   console.log('[Deploy] Deploying platform...');
-  platform = await Platform.deploy(coordinator.address, subscriptionId, KEY_HASH);
+  platform = await Platform.deploy();
   console.log('[Deploy] Deploy tx send...');
 
   await platform.deployed();
 
-  if (hre.network.name === 'localhost') {
-    console.log('[Deploy] Platform deployed...');
-    console.log('[Deploy] Adding a consumer...');
-
-    await (
-      await coordinator.connect(vrfAdmin).addConsumer(subscriptionId, platform.address, { gasLimit: 300000})
-    ).wait();
-
-    console.log('[Deploy] Consumer added...');
-  }
   console.log('[Deploy] Saving deploy artifacts...');
 
   saveFrontendFiles(platform);
 
   console.log(
-    `[Deploy] DONE!\nPlatform with coordinator "${coordinator.address}", subscription ID "${subscriptionId}", and key hash of "${KEY_HASH}" deployed to ${platform.address}`
+    `[Deploy] DONE!\nPlatform deployed to ${platform.address}`
   );
 }
 
