a38f503ebbc69f1cafa090871c27f81779fb9a98
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-08-15 11:44:08 +0300

    Remove HH task for fulfilling VRF requests

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,40 +4,6 @@ require('@oasisprotocol/sapphire-hardhat');
 require('dotenv').config();
 
 task(
-  "vrf_fulfill",
-  "Fulfills VRF random number request"
-).
-  addPositionalParam('requestId').
-  addPositionalParam('value').
-  setAction(async ({ requestId, value }, hre, runSuper) => {
-    const contractAddresses = require('./build/contract-addresses.json');
-
-    // TODO: possibly reuse most of this tasks logic in tests
-    const { VRF_COORDINATOR } = process.env;
-
-    const Platform = await ethers.getContractFactory("Platform");
-    const platform = await Platform.attach(contractAddresses.Platform);
-
-    const someEther = ethers.utils.parseEther('0.3');
-
-    // Give coordinator some ETH
-    await network.provider.send(
-      "hardhat_setBalance",
-      [
-        VRF_COORDINATOR,
-        someEther.toHexString().replace("0x0", "0x")
-      ]
-    );
-
-    // TODO: in dev. environment -> take this coordinator address from the deploy artifacts
-    const impersonatedCoordinator = await ethers.getImpersonatedSigner(VRF_COORDINATOR);
-
-    await platform.connect(impersonatedCoordinator).rawFulfillRandomWords(requestId, [value], { gasLimit: 300000 });
-    console.log(`Fulfilled a request (ID=${requestId}) with value ${value}`);
-  }
-);
-
-task(
   'add_subscription_plan',
   'Add a subscription plan'
 ).addPositionalParam('price').addPositionalParam('duration').setAction(
