dbbc73b13fe51610f98b3169cafb69c6db0c442f
Kristians Kuhta <kristians-kuhta@users.noreply.github.com>
2023-09-10 17:49:09 +0300

    Add song voting feature and partial tests

---
+++ contracts/SongVoting.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { SharedStorage } from "./SharedStorage.sol";
+
+contract SongVoting is Ownable, SharedStorage {
+  // NOTE: Period being 0 means that voting is not active.
+  //       This is the last timestamp that you can vote.
+  uint256 internal votingPeriodEnds;
+
+  mapping(uint256 songId => uint256 votes) internal songVotes;
+
+  uint256[] internal songsWithVotes;
+  uint256 internal songsWithVotesCount;
+  address[] internal voters;
+  uint256 internal votersCount;
+
+  error VotingActive();
+  error VotingNotActive();
+  error VotingIsAlreadyOpen();
+  error VotingHasNotEnded();
+
+  error RewardsPerProposalZero();
+  error TimestampMustBeInFuture();
+
+  event VotingOpen();
+  event VotingClosed(uint256 winningSongId);
+
+  function openVotingPeriod(uint256 endTimestamp) external onlyOwner {
+    _requireInactiveVotingPeriod();
+    _requireTimestampInFuture(endTimestamp);
+
+    votingPeriodEnds = endTimestamp;
+
+    _resetVotingStorage();
+
+    emit VotingOpen();
+  }
+
+  function vote(uint256 _songId) external {
+    _requireActiveVotingPeriod();
+
+    if (songVotes[_songId] == 0) {
+      _insertSongsWithVotes(_songId);
+    }
+
+    songVotes[_songId]++;
+
+    _insertVoter(msg.sender);
+  }
+
+  function closeVotingPeriod() external {
+    _requireVotingPeriodEnded();
+
+    votingPeriodEnds = 0;
+
+    _distributeVotingRewards();
+    uint256 winningSongId = _winningSongId();
+
+    emit VotingClosed(winningSongId);
+  }
+
+  function isVotingPeriodActive() external view returns (bool) {
+    return _isVotingPeriodActive();
+  }
+
+  function _insertSongsWithVotes(uint256 songId) internal {
+    // Array needs to be expanded
+    if (songsWithVotes.length == songsWithVotesCount) {
+      songsWithVotes.push(songId);
+    } else {
+      songsWithVotes[songsWithVotesCount] = songId;
+    }
+
+    songsWithVotesCount++;
+  }
+
+  function _insertVoter(address voter) internal {
+    // Array needs to be expanded
+    if (voters.length == votersCount) {
+      voters.push(voter);
+    } else {
+      voters[votersCount] = voter;
+    }
+
+    votersCount++;
+  }
+
+  function _distributeVotingRewards() internal {
+    for(uint256 i=0; i < votersCount; i++) {
+      address voter = voters[i];
+      uint256 reward = _rewardTokensPerVoter();
+
+      rewardsToken.mint(voter, reward);
+    }
+  }
+
+  function _resetVotingStorage() internal {
+    for(uint256 i=0; i < songsWithVotesCount; i++) {
+      uint256 songId = songsWithVotes[i];
+      songVotes[songId] = 0;
+    }
+
+    votersCount = 0;
+    songsWithVotesCount = 0;
+  }
+
+  function _winningSongId() internal view returns (uint256 songId) {
+    uint256 maxVotes;
+
+
+    for(uint256 i=0; i < songsWithVotesCount; i++) {
+      uint256 currentSongId = songsWithVotes[i];
+      uint256 votesForSong = songVotes[currentSongId];
+
+      if (votesForSong > maxVotes) {
+        maxVotes = votesForSong;
+        songId = currentSongId;
+      }
+    }
+  }
+
+  function _requireTimestampInFuture(uint256 timestamp) internal view {
+    if (timestamp <= block.timestamp) {
+      revert TimestampMustBeInFuture();
+    }
+  }
+
+  function _rewardTokensPerVoter() internal view returns (uint256) {
+    // NOTE: this probably results in voters being paid out just a little under
+    //       what they should be paid out, but the difference should be really small
+    return rewardsPerProposal / votersCount;
+  }
+
+  function _requireVotingPeriodEnded() internal view {
+    if (block.timestamp > votingPeriodEnds && votingPeriodEnds < 0) {
+      revert VotingHasNotEnded();
+    }
+  }
+
+  function _requireActiveVotingPeriod() internal view {
+    if (!_isVotingPeriodActive()) {
+      revert VotingNotActive();
+    }
+  }
+
+  function _requireInactiveVotingPeriod() internal view {
+    if (_isVotingPeriodActive()) {
+      revert VotingActive();
+    }
+  }
+
+  function _requireVotingRewards() internal view {
+    if (rewardsPerProposal == 0) {
+      revert RewardsPerProposalZero();
+    }
+  }
+
+  function _isVotingPeriodActive() internal view returns (bool) {
+    return votingPeriodEnds >= block.timestamp;
+  }
+}
---
+++ test/SongVoting.js
@@ -0,0 +1,149 @@
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { loadFixture, time } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { BigNumber } = ethers;
+const {
+  deployPlatform,
+  registerSong,
+  registerArtist,
+} = require('./utils');
+
+describe('Song voting', function() {
+  describe('Opening of voting period', function () {
+    it('reverts when opening voting period with end timestamp that is not in future', async () => {
+      const { platform } = await loadFixture(deployPlatform);
+
+      const timestamp = await time.latest();
+
+      // NOTE: next block timestamp will be timestamp + 1
+      await expect(
+        platform.openVotingPeriod(timestamp)
+      ).to.be.revertedWithCustomError(platform, 'TimestampMustBeInFuture');
+    });
+
+    it('reverts when opening voting period and previous period is not closed', async () => {
+      const { platform } = await loadFixture(deployPlatform);
+
+      const one = BigNumber.from(1);
+      const two = BigNumber.from(2);
+
+      const currentTimestampOne = BigNumber.from(await time.latest());
+      const timestampOne = currentTimestampOne.add(two);
+
+      await time.setNextBlockTimestamp(currentTimestampOne.add(one));
+
+      await (await platform.openVotingPeriod(timestampOne)).wait();
+
+      const currentTimestampTwo = BigNumber.from(await time.latest());
+      const timestampTwo = currentTimestampTwo.add(two);
+
+      await time.setNextBlockTimestamp(currentTimestampTwo.add(one));
+
+      await expect(
+        platform.openVotingPeriod(timestampTwo)
+      ).to.be.revertedWithCustomError(platform, 'VotingActive');
+    });
+
+    it('reverts when opening voting period by non-owner', async () => {
+      const { platform, secondAccount } = await loadFixture(deployPlatform);
+
+      const timestamp = await time.latest();
+
+      // NOTE: timestamp does not matter here
+      await expect(
+        platform.connect(secondAccount).openVotingPeriod(timestamp)
+      ).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('opens a voting period', async () => {
+      const { platform } = await loadFixture(deployPlatform);
+
+      const one = BigNumber.from(1);
+      const two = BigNumber.from(2);
+
+      const currentTimestamp = BigNumber.from(await time.latest());
+      const timestamp = currentTimestamp.add(two);
+
+      await time.setNextBlockTimestamp(currentTimestamp.add(one));
+
+      await expect(
+        platform.openVotingPeriod(timestamp)
+      ).to.emit(platform, 'VotingOpen');
+
+      expect(await platform.isVotingPeriodActive()).to.eq(true);
+    });
+
+    it('cleans up previous votes when opening voting period after closing one', async () => {
+      const { platform, firstAccount, secondAccount } = await loadFixture(deployPlatform);
+
+      const one = BigNumber.from(1);
+      const hundred = BigNumber.from(100);
+
+      const currentTimestampOne = BigNumber.from(await time.latest());
+      const timestampOne = currentTimestampOne.add(hundred);
+
+      await time.setNextBlockTimestamp(currentTimestampOne.add(one));
+
+      await (await platform.openVotingPeriod(timestampOne)).wait();
+
+      // register an artist
+      await registerArtist(platform, firstAccount, 'First Artist');
+
+      // register a song
+      const firstIpfsID = 'QmPK1s3pNYLi9ERiq3BDxKa4XosgWwFRQUydHUtz4YgpqB';
+      const secondIpfsID = 'Qm22222pNYLi9ERiq3BDxKa4XosgWwFRQUydHUtz422222';
+
+      const firstSongId = await registerSong(platform, firstAccount, firstIpfsID, 0);
+      const secondSongId = await registerSong(platform, firstAccount, secondIpfsID, 0);
+
+      // vote for the song
+      await (await platform.vote(firstSongId)).wait();
+      await (await platform.connect(secondAccount).vote(firstSongId)).wait();
+
+      // close voting period
+      await (await platform.closeVotingPeriod()).wait();
+
+      const currentTimestampTwo = BigNumber.from(await time.latest());
+      const timestampTwo = currentTimestampTwo.add(hundred);
+
+      await time.setNextBlockTimestamp(currentTimestampTwo.add(one));
+
+      await expect(
+        platform.openVotingPeriod(timestampTwo)
+      ).to.emit(platform, 'VotingOpen');
+
+      // expect voting period opening to erase previous voting data
+      expect(await platform.isVotingPeriodActive()).to.eq(true);
+
+      await (await platform.vote(secondSongId)).wait();
+
+      await expect(
+        platform.closeVotingPeriod()
+      ).to.emit(platform, 'VotingClosed').withArgs(secondSongId);
+    });
+  });
+
+  describe('Voting', function () {
+    it('reverts when voting and voting period is not active', async () => {
+
+    });
+    it('reverts when trying to vote for same song twice', async () => {
+
+    });
+    it('adds a vote for a song', async () => {
+
+    });
+  });
+
+  describe('Closing of voting period', function () {
+    it('reverts when closing voting period with that has not ended yet', async () => {
+
+    });
+    it('reverts when closing voting period without', async () => {
+
+    });
+    it('closes a voting period', async () => {
+
+    });
+  });
+});
